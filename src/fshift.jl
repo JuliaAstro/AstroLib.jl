b72219deb2e1b7778da7caf4d62ceb08a768b761
Michael-Reefe <michael.reefe8@gmail.com>
2024-10-02 01:12:10 -0400

    fixed jldoctests

--- src/fshift.jl
+++ src/fshift.jl
@@ -55,8 +55,8 @@ y coordinates:
 
 ```jldoctest
 julia> using AstroLib
-julia> im = [x+y for x in 1:10, y in 1:10];
-julia> fshift(im, 0.5, 0.5)
+julia> image = [x+y for x in 1:10, y in 1:10];
+julia> fshift(image, 0.5, 0.5)
 10×10 Matrix{Float64}:
  11.0   7.0   8.0   9.0  10.0  11.0  12.0  13.0  14.0  15.0
   7.0   3.0   4.0   5.0   6.0   7.0   8.0   9.0  10.0  11.0
